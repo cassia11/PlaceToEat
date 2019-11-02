fa0d49d8fa7afc4c522e88d2a9be96c8cf5433be
ANA LUCIA BOLICO <loliveira_ana@terceiros.sicred.com.br>
2019-11-01 23:57:17 -0300

    first commit

---
+++ src/main/java/br/com/commida/CommidaApplication.java
@@ -0,0 +1,13 @@
+package br.com.commida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommidaApplication.class, args);
+	}
+
+}
